d8f3ed68414e5fc8a43683decc06f46c78930b63
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-02-09 10:57:54 +0530

    9-2-26

--- index.js
+++ index.js
@@ -1,73 +1,118 @@
-console.log("amrit")
-
 const express = require("express");
-
 const app = express();
 
-
 const users = [
-  { id: 1, name: "Arjun", role: "student" },
-  { id: 2, name: "Priyesha", role: "mentor" }
+    {
+        studentName: "ABDUL HAQUE",
+        University: "SUxCG 714",
+        UniversityUID: "108444"
+    },
+    {
+        studentName: "ADITYA KUMAR",
+        University: "SUxCG 702",
+        UniversityUID: "108716",
+    },
+    {
+        studentName: "AMAN KUMAR",
+        University: "SUxCG 702",
+        UniversityUID: "108500"
+    },
+    {
+        studentName: "AMRIT RAJ",
+        University: "SUxCG 702",
+        UniversityUID: "108587"
+    }
 ];
+
 app.get("/", (req, res) => {
-  res.send("Express server is run by  dino");
+    res.send("my name is Amrit Raj Kushwaha");
 });
 
-app.get("/users", (req, res) => {
-  res.status(200).json(users);
-});
-
-app.get("/users/:id", (req, res) => {
-  const userId = Number(req.params.id);
-  const user = users.find(u => u.id === userId);
-
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  res.status(200).json(user);
-});
-app.use(express.json());
-app.post("/users", (req, res) => {
-  console.log(req.body);
-  const newUser = {
-    id: users.length + 1,
-    name: req.body[0].name,
-    role: req.body[0].role
-  };
-
-  users.push(newUser);
-
-  res.status(201).json({
-    message: "User created",
-    user: newUser
-  });
-});
-
-app.put("/users/:id", (req, res) => {
-  const userId = Number(req.params.id);
-  const index = users.findIndex(u => u.id === userId);
-
-  if (index === -1) {
-    return res.status(404).json({ message: "not found" });
-  }
-
-  users[index] = {
-    id: userId,
-    name: req.body.name,
-    role: req.body.role
-  };
-
-  res.status(200).json({
-    message: "User replaced",
-    user: users[index]
-  });
+app.get("/cg", (req, res) => {
+    res.status(200).json(users);
 });
 
 
-app.listen(4000, () => {
-  console.log("Server started on port 4000");
+app.get("/cg/:students", (req, res) => {
+    const name = req.params.students;
+
+    const user = users.find(u => u.studentName === name);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+});
+
+
+app.get("/cg/:students/:gr_number", (req, res) => {
+    console.log("params:", req.params);
+    res.status(200).json(req.params);
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
 });
 
 
 
+
+
+// const express = require("express");
+
+// const app = express();
+
+// const users = [
+//     {
+//         "studentName": "ABDUL HAQUE",
+//         "University": "SUxCG 714",
+//         "UniversityUID": "108444"
+//     },
+//     {
+//         "studentName": "ADITYA KUMAR",
+//         "University": "SUxCG 702",
+//         "UniversityUID": "108716",
+//     },
+//     {
+//         "studentName": "AMAN KUMAR",
+//         "University": "SUxCG 702",
+//         "UniversityUID": "108500"
+//     },
+//     {
+//         "studentName": "AMRIT RAJ",
+//         "University": "SUxCG 702",
+//         "UniversityUID": "108587"
+//     }
+// ];
+
+// app.get("/", (req, res) => {
+//     res.send("my name is Amrit Raj Kushwaha");
+// });
+
+// app.get("/cg", (req, res) => {
+//     res.status(200).json(users);
+// });
+
+// app.get("/cg/:students/:gr_number", (req, res) => {
+//     console.log("this is req :", req.params);
+//     console.log("this is req :", req);
+//     res.status(200).json(users);
+// });
+
+// app.get("/cg/:students", (req, res) => {
+//     const name = req.params.studentName;
+
+//     const users = users.find(u => u.studentName === name);
+
+//     if (!users) {
+//         return res.status(404).json({ message: "User not found" });
+//     }
+
+//     res.status(200).json(users);
+// });
+
+
+// app.listen(3000, () => {
+//     console.log("Server started on port 3000");
+// });
